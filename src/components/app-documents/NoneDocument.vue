bea82bb1464b0f0d404e63f9f34e1a6f78e976c2
Victor Pereira <ovictorpereira@gmail.com>
2025-07-01 14:29:18 -0300

    Refactor: update NoneDocument component to display vscode logo with improved layout

--- src/components/app-documents/NoneDocument.vue
+++ src/components/app-documents/NoneDocument.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>nenhum</div>
+  <div class="none-content">
+    <img src="@/assets/images/vscode-black.svg" alt="vscode-logo" />
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.none-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.none-content img {
+  width: 200px;
+}
+</style>
