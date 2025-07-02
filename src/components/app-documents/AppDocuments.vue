f26ea0100b26b0d5babc104d65b65137f04d6341
Victor Pereira <ovictorpereira@gmail.com>
2025-07-02 17:47:17 -0300

    Refactor: replace ReadmeReader with MarkdownReader component; update import paths and add TabsContainer component

--- src/components/app-documents/AppDocuments.vue
+++ src/components/app-documents/AppDocuments.vue
@@ -4,15 +4,15 @@
     <div class="tab-content vertical-scrollbar">
       <NoneDocument v-if="documents.length === 0" />
 
-      <ReadmeReader :content="activeDocument?.content" :readme="isReadmeActive" v-else />
+      <MarkdownReader :content="activeDocument?.content" :readme="isReadmeActive" v-else />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import TabsContainer from './TabsContainer.vue'
-import NoneDocument from './NoneDocument.vue'
-import ReadmeReader from './ReadmeReader.vue'
+import TabsContainer from './components/TabsContainer.vue'
+import NoneDocument from './components/NoneDocument.vue'
+import MarkdownReader from './components/MarkdownReader.vue'
 import { computed } from 'vue'
 import { useDocumentsStore } from '@/stores/documents'
 
