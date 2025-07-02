a0d9c10fd14ab9d59644765a9417c1a055c40cde
Victor Pereira <ovictorpereira@gmail.com>
2025-07-02 12:00:09 -0300

    Refactor: simplify WindowControls layout; remove redundant class names; enhance full-screen toggle functionality

--- src/components/taskbar/WindowControls.vue
+++ src/components/taskbar/WindowControls.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="window-controls">
-    <div class="window-control window-control-minimize">
+    <div class="window-control">
       <svg width="16" height="16" viewBox="0 0 12 12">
         <path d="M2 6h8v1H2z" fill="currentColor" />
       </svg>
     </div>
 
-    <div class="window-control window-control-maximize" @click="toggleFullScreen">
-      <svg width="16" height="16" viewBox="0 0 12 12">
+    <div class="window-control" @click="toggleFullScreen">
+      <svg width="16" height="16" viewBox="0 0 12 12" v-if="isFullScreen">
+        <rect
+          x="3"
+          y="1"
+          width="7"
+          height="7"
+          stroke="currentColor"
+          stroke-width="1"
+          rx="1"
+          fill="#181818"
+        />
+
+        <rect
+          x="1"
+          y="3"
+          width="7"
+          height="7"
+          stroke="currentColor"
+          stroke-width="1"
+          rx="1"
+          fill="#181818"
+        />
+      </svg>
+
+      <svg width="16" height="16" viewBox="0 0 12 12" v-else>
         <path d="M2 2v8h8V2H2zm7 7H3V3h6v6z" fill="currentColor" />
       </svg>
     </div>
 
-    <div class="window-control window-control-close">
+    <div class="window-control">
       <svg width="12" height="12" viewBox="0 0 12 12">
         <path
           d="M7.5 6l3.7 3.7-1.5 1.5L6 7.5 2.3 11.2.8 9.7 4.5 6 .8 2.3 2.3.8 6 4.5 9.7.8l1.5 1.5L7.5 6z"
@@ -25,9 +49,9 @@
 
 <script setup lang="ts">
 import { useTemplateStore } from '@/stores/template'
-// import { computed } from 'vue'
+import { computed } from 'vue'
 const templateStore = useTemplateStore()
-// const isFullScreen = computed(() => templateStore.vsCodeConfig.isFullScreen)
+const isFullScreen = computed(() => templateStore.vsCodeConfig.isFullScreen)
 const toggleFullScreen = () => {
   templateStore.vsCodeConfig.isFullScreen = !templateStore.vsCodeConfig.isFullScreen
 }
