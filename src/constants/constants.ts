83e5bdcb3316391ed792238eae9d58936968cee6
Victor Pereira <ovictorpereira@gmail.com>
2025-07-22 14:36:34 -0300

    Refactor: update PROJECT_NAME constant to match the correct template name

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,3 @@
-export const PROJECT_NAME = 'vscode-resume-template'
+export const PROJECT_NAME = 'vue-vscode-cv-template'
 export const GITHUB_USERNAME = 'ovictorpereira'
 export const TERMINAL_PATH = `D:\\Projects\\${PROJECT_NAME}`
